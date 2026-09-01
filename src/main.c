4ae7086564a547c606768e102df8b01a009dabe0
Phil Armstead <philarmstead@mailbox.org>
2026-09-02 00:08:04 +0100

    Closing windows via the keyboard

--- src/main.c
+++ src/main.c
@@ -24,6 +24,7 @@ int main(const int argc, char **argv) {
 	logger_init();
 
 	GtkApplication *app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+	gameContext.app = app;
 	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 	const int status = g_application_run(G_APPLICATION(app), argc, argv);
 	g_object_unref(app);
